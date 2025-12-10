708f07b892c836025acd3abd06ca641dcfbf3dc7
fahmiabaz99 <fahmi10222168@sttcipasung.ac.id>
2025-12-10 19:39:20 +0700

    first commit

--- app.py
+++ app.py
@@ -1,24 +1,25 @@
 import joblib
-import streamlit as st 
+import streamlit as st
 
-model = joblib.load("models/model_logistic_reggression.pkl")
+model = joblib.load("models/model_logistic_regression.pkl")
 tfidf = joblib.load("models/tfidf_vectorizer.pkl")
 
-st.title("Aplikasi Klasifikas Komentar")
-st.write("Aplikasi Gabut Aja Ini Mah")
-input.text_area("Masukan Komentar Anda!")
+st.title("Aplikasi Klasifikasi Komentar")
+st.write("Aplikasi ini merupakan implementasi model NLP menggunakan Logistic Regression")
 
-if st.buttom("Prediksi"):
+input = st.text_area("Masukkan Komentar Anda!")
+
+if st.button("Prediksi"):
     if input.strip() == "":
-        st.warning("Tolong Komentar Jangan Kosong")
+        st.warning("Komentar Tidak Boleh Kosong")
     else:
         vector = tfidf.transform([input])
-        prediction = model.prdict(vector)[0]
-
+        prediction = model.predict(vector)[0]
+        
         label_mapping = {
-            0: "Negatif"
+            0: "Negatif",
             1: "Positif"
         }
+        
         st.subheader("Hasil Analisis Komentar")
         st.write("**Sentiment Anda:**", label_mapping.get(prediction, prediction))
-        
